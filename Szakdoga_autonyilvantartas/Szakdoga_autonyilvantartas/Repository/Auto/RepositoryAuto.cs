987c0ee3593cd6cf751310acf724496537ffd608
Marton Zoltan <martonzoli99@gmail.com>
2020-02-16 13:50:11 +0100

    Repo és Repo auto létrehozva

---
+++ Szakdoga_autonyilvantartas/Szakdoga_autonyilvantartas/Repository/Auto/RepositoryAuto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Szakdoga_autonyilvantartas.Repository;
+
+namespace Szakdoga_autonyilvantartas.Repository.Auto
+{
+    class RepositoryAuto
+    {
+        List<Auto> autok;
+
+        public List<Auto> getAutok()
+        {
+            return autok;
+        }
+    }
+}
---
+++ Szakdoga_autonyilvantartas/Szakdoga_autonyilvantartas/Repository/Repository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Szakdoga_autonyilvantartas.Repository;
+
+namespace Szakdoga_autonyilvantartas.Repository
+{
+    class Repository
+    {
+        autok = new List<Auto>();
+    }
+}
